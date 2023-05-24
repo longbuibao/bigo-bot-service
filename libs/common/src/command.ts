4e2212d250716908fb43ae9abfa21b4bc53f73fa
long bui <blong1102@gmail.com>
2023-05-24 22:43:38 +0700

    feat: add utils and common libs

---
+++ libs/common/src/command.ts
@@ -0,0 +1,2 @@
+export const GET_IAM_USERS = 'get_iam_users'
+export const CREATE_EC2_INSTANCE = 'create_ec2_instance'
---
+++ libs/common/src/utils.ts
@@ -0,0 +1,4 @@
+export const countNumberOfInstanceBaseOnView = (numberOfView: number): number => {
+  // some how calculate the number of instance base on the view and the available of accounts with status free
+  return 3
+}
