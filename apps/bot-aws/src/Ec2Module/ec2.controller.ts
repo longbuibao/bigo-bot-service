0faad92a59505d40d91b97e6fbbb5597b237a4b5
long bui <blong1102@gmail.com>
2023-05-27 13:17:00 +0700

    feat: remove get Iam user

--- apps/bot-aws/src/Ec2Module/ec2.controller.ts
+++ apps/bot-aws/src/Ec2Module/ec2.controller.ts
@@ -1,23 +1,17 @@
 import { Controller } from '@nestjs/common'
 import { Ec2Service } from './ec2.service'
 import { MessagePattern, Payload } from '@nestjs/microservices'
-import { ListUsersCommandOutput } from '@aws-sdk/client-iam'
-import { RunInstancesCommandOutput } from '@aws-sdk/client-ec2'
+import { Instance } from '@aws-sdk/client-ec2'
 
 import { CreateEc2InstanceDto } from '@bigo-bot/common/dtos/CreateEc2InstanceDto'
-import { GET_IAM_USERS, CREATE_EC2_INSTANCE } from '@bigo-bot/common/command'
+import { CREATE_EC2_INSTANCE } from '@bigo-bot/common/command'
 
 @Controller()
 export class Ec2Controller {
   constructor (private readonly ec2Service: Ec2Service) {}
 
-  @MessagePattern({ cmd: GET_IAM_USERS })
-  async getIamUsers (): Promise<ListUsersCommandOutput> {
-    return await this.ec2Service.getIamUsers()
-  }
-
   @MessagePattern({ cmd: CREATE_EC2_INSTANCE })
-  async createEc2Instances (@Payload() createEc2InstanceDto: CreateEc2InstanceDto): Promise<RunInstancesCommandOutput> {
-    return await this.ec2Service.createEc2Instances(createEc2InstanceDto)
+  async createEc2Instances (@Payload() createEc2InstanceDto: CreateEc2InstanceDto): Promise<Instance[]> {
+    return await this.ec2Service.spinUpEc2Bots(createEc2InstanceDto)
   }
 }
--- apps/bot-aws/src/Ec2Module/ec2.service.ts
+++ apps/bot-aws/src/Ec2Module/ec2.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common'
-import { IAMClient, ListUsersCommand, ListUsersCommandOutput, ListUsersCommandInput } from '@aws-sdk/client-iam'
+
+import { WaiterState } from '@aws-sdk/util-waiter'
 import {
   RunInstancesCommandOutput,
   RunInstancesCommand,
   EC2Client,
   RunInstancesCommandInput,
   waitUntilInstanceRunning,
-  DescribeInstancesCommand
+  DescribeInstancesCommand,
+  Instance
 } from '@aws-sdk/client-ec2'
 
 import { CreateEc2InstanceDto } from '@bigo-bot/common/dtos/CreateEc2InstanceDto'
@@ -14,13 +16,7 @@ import { countNumberOfInstanceBaseOnView } from '@bigo-bot/common/utils'
 
 @Injectable()
 export class Ec2Service {
-  async getIamUsers (): Promise<ListUsersCommandOutput> {
-    const client = new IAMClient({ region: 'us-east-1', apiVersion: '2010-05-08' })
-    const listUsersRequest = new ListUsersCommand({})
-    return await client.send<ListUsersCommandInput, ListUsersCommandOutput>(listUsersRequest)
-  }
-
-  async createEc2Instances (createEc2InstanceDto: CreateEc2InstanceDto): Promise<RunInstancesCommandOutput> {
+  async spinUpEc2Bots (createEc2InstanceDto: CreateEc2InstanceDto): Promise<Instance[]> {
     const client = new EC2Client({ region: createEc2InstanceDto.region })
     const numberOfInstance = countNumberOfInstanceBaseOnView(createEc2InstanceDto.viewAmount)
 
@@ -32,28 +28,51 @@ export class Ec2Service {
       MaxCount: numberOfInstance
     })
 
-    const instances = await client.send<RunInstancesCommandInput, RunInstancesCommandOutput>(command)
-    await this.sshToEc2Instance(instances, client)
+    const response = await client.send<RunInstancesCommandInput, RunInstancesCommandOutput>(command)
+    const instances = await this.getInstanceWithRunningState(response.Instances as Instance[], client)
     return instances
   }
 
-  async sshToEc2Instance (instances: RunInstancesCommandOutput, ec2Client: EC2Client): Promise<void> {
-    if (instances.Instances !== undefined) {
-      const instanceIds = instances.Instances.map(instance => instance.InstanceId) as string[]
-      await waitUntilInstanceRunning({ client: ec2Client, maxWaitTime: 600 }, { InstanceIds: instanceIds })
-      const command = new DescribeInstancesCommand({})
-      const response = await ec2Client.send(command)
+  async waitForInstanceHasRunningState (instances: Instance[], ec2Client: EC2Client): Promise<WaiterState> {
+    const instanceIds = instances.map(instance => instance.InstanceId) as string[]
+    const waitResult = await waitUntilInstanceRunning({ client: ec2Client, maxWaitTime: 600 }, { InstanceIds: instanceIds })
+    return waitResult.state
+  }
 
-      const instancesAfterWaitForItRunning = response.Reservations?.flatMap((reservation) => reservation.Instances) ?? []
+  async getInstanceWithRunningState (instances: Instance[], ec2Client: EC2Client): Promise<Instance[]> {
+    const state = await this.waitForInstanceHasRunningState(instances, ec2Client)
+    if (state === WaiterState.SUCCESS) {
+      const startTime = new Date()
+      startTime.setMinutes(startTime.getMinutes() - 3)
 
-      for (const instance of instancesAfterWaitForItRunning) {
-        if (instance !== undefined) {
-          const instanceId = instance.InstanceId ?? 'Unknown'
-          const instanceStatus = instance.State?.Name ?? 'Unknown'
-
-          console.log(`Instance ID: ${instanceId}, Status: ${instanceStatus}`)
+      try {
+        const timeFilter = {
+          Name: 'launch-time',
+          Values: [
+            startTime.toISOString(),
+            new Date().toISOString()
+          ]
         }
+
+        const stateFilter = {
+          Name: 'instance-state-name',
+          Values: ['running']
+        }
+
+        const command = new DescribeInstancesCommand({
+          Filters: [timeFilter, stateFilter]
+        })
+
+        const response = (await ec2Client.send(command)).Reservations
+        if (response !== undefined) {
+          const instances = response.flatMap((reservation) => reservation.Instances) as Instance[]
+          return instances
+        }
+      } catch (error) {
+        console.error('Error:', error)
       }
     }
+
+    return []
   }
 }
