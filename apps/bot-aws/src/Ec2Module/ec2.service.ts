0679e11477e82f26e1744203203824c9e1c7560f
long bui <blong1102@gmail.com>
2023-05-27 16:02:29 +0700

    chore: clean up

--- apps/bot-aws/src/Ec2Module/ec2.service.ts
+++ apps/bot-aws/src/Ec2Module/ec2.service.ts
@@ -18,25 +18,14 @@ import { bigoLog } from '@bigo-bot/common/log'
 
 @Injectable()
 export class Ec2Service {
+  private readonly ec2Client: EC2Client
+  constructor () {
+    this.ec2Client = new EC2Client({ region: 'us-east-1' })
+  }
+
   async spinUpEc2Bots (createEc2InstanceDto: CreateEc2InstanceDto): Promise<CreateEc2InstanceResult[]> {
-    const client = new EC2Client({ region: createEc2InstanceDto.region })
-    const numberOfInstance = countNumberOfInstanceBaseOnView(createEc2InstanceDto.viewAmount)
-
-    // todo: some how pass the idol url and the bot accounts array
-    const command = new RunInstancesCommand({
-      ImageId: createEc2InstanceDto.imageId,
-      InstanceType: createEc2InstanceDto.instanceType,
-      MinCount: 1,
-      MaxCount: numberOfInstance
-    })
-
-    bigoLog(`Creating ${numberOfInstance} EC2 Instances with Idol ${createEc2InstanceDto.idolUrl}`)
-    const response = await client.send<RunInstancesCommandInput, RunInstancesCommandOutput>(command)
-    bigoLog(`Created ${numberOfInstance} instances!`)
-
-    bigoLog('Waiting for EC2 instances running...')
-    const instances = await this.getInstanceWithRunningState(response.Instances as Instance[], client)
-
+    const response = await this.createInstance(createEc2InstanceDto)
+    const instances = await this.getInstanceWithRunningState(response.Instances as Instance[])
     return instances.map((instance: Instance) => {
       const createResult: CreateEc2InstanceResult = {
         InstanceId: instance.InstanceId,
@@ -49,46 +38,60 @@ export class Ec2Service {
     })
   }
 
-  async waitForInstanceHasRunningState (instances: Instance[], ec2Client: EC2Client): Promise<WaiterState> {
+  private async waitForInstanceHasRunningState (instances: Instance[]): Promise<WaiterState> {
     const instanceIds = instances.map(instance => instance.InstanceId) as string[]
-    const waitResult = await waitUntilInstanceRunning({ client: ec2Client, maxWaitTime: 600 }, { InstanceIds: instanceIds })
+    const waitResult = await waitUntilInstanceRunning({ client: this.ec2Client, maxWaitTime: 600 }, { InstanceIds: instanceIds })
     return waitResult.state
   }
 
-  async getInstanceWithRunningState (instances: Instance[], ec2Client: EC2Client): Promise<Instance[]> {
-    const state = await this.waitForInstanceHasRunningState(instances, ec2Client)
+  private async getInstanceWithRunningState (instances: Instance[]): Promise<Instance[]> {
+    bigoLog('Waiting for EC2 instances with running state...')
+
+    const state = await this.waitForInstanceHasRunningState(instances)
     if (state === WaiterState.SUCCESS) {
       const startTime = new Date()
       startTime.setMinutes(startTime.getMinutes() - 3)
 
       try {
-        // const timeFilter = {
-        //   Name: 'launch-time',
-        //   Values: [
-        //     startTime.toISOString(),
-        //     new Date().toISOString()
-        //   ]
-        // }
-
-        // const stateFilter = {
-        //   Name: 'instance-state-name',
-        //   Values: ['running']
-        // }
+        const stateFilter = {
+          Name: 'instance-state-name',
+          Values: ['running']
+        }
 
         const command = new DescribeInstancesCommand({
-          // Filters: [timeFilter, stateFilter]
+          Filters: [stateFilter]
         })
 
-        const response = (await ec2Client.send(command)).Reservations
+        const response = (await this.ec2Client.send(command)).Reservations
         if (response !== undefined) {
           const instances = response.flatMap((reservation) => reservation.Instances) as Instance[]
           return instances
         }
+
+        bigoLog('Done created instances')
       } catch (error) {
         console.error('Error:', error)
       }
     }
-
+    bigoLog('Done created instances')
     return []
+  }
+
+  private async createInstance (createEc2InstanceDto: CreateEc2InstanceDto): Promise<RunInstancesCommandOutput> {
+    const numberOfInstance = countNumberOfInstanceBaseOnView(createEc2InstanceDto.viewAmount)
+    bigoLog(`Creating ${numberOfInstance} EC2 Instances with Idol ${createEc2InstanceDto.idolUrl}`)
+
+    // todo: some how pass the idol url and the bot accounts array
+    const command = new RunInstancesCommand({
+      ImageId: createEc2InstanceDto.imageId,
+      InstanceType: createEc2InstanceDto.instanceType,
+      MinCount: 1,
+      MaxCount: numberOfInstance
+    })
+
+    const response = await this.ec2Client.send<RunInstancesCommandInput, RunInstancesCommandOutput>(command)
+    bigoLog(`Created ${numberOfInstance} instances!`)
+
+    return response
   }
 }
--- libs/common/src/log.ts
+++ libs/common/src/log.ts
@@ -1,9 +1,23 @@
 import * as chalk from 'chalk'
 
+const getCurrentTimestamp = (): string => {
+  const now = new Date()
+  const month = (now.getMonth() + 1).toString().padStart(2, '0')
+  const day = now.getDate().toString().padStart(2, '0')
+  const year = now.getFullYear().toString()
+  const hours = now.getHours()
+  const minutes = now.getMinutes().toString().padStart(2, '0')
+  const seconds = now.getSeconds().toString().padStart(2, '0')
+  const period = hours >= 12 ? 'PM' : 'AM'
+
+  const timestamp = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${period}`
+  return timestamp
+}
+
 export const bigoLog = (message: string): void => {
-  console.log(`${chalk.bgGreen('[BIGO_LOG]') as string} ${chalk.green(message) as string}`)
+  console.log(`${chalk.bgGreen('[BIGO_LOG]') as string} - ${getCurrentTimestamp()} \t ${chalk.green(message) as string}`)
 }
 
 export const bigoLogError = (message: string): void => {
-  console.log(`${chalk.bgRed('[BIGO_LOG]') as string} ${chalk.red(message) as string}`)
+  console.log(`${chalk.bgRed('[BIGO_LOG]') as string} - ${getCurrentTimestamp()} \t ${chalk.red(message) as string}`)
 }
